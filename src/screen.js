b53c8542227f91c8d158f018eb33f15e2f1fddd9
DAKSHTECHIE1 <daksh13579@gmail.com>
2023-01-09 13:40:10 +0530

    Improve the  UI

--- src/screen.js
+++ src/screen.js
@@ -1,40 +1,9 @@
 import React from "react";
 import Screeninside from './screeninside';
 const Screen=(props)=>{
-    if(props.screen===0){  
         return(
         <div className="screen" >
             <Screeninside active={props.active} updateRender={props.updateRender} screen={props.screen}/>
         </div>)
-    }
-    else if(props.screen===1){
-        return(
-            <div className="screen">
-               <div className="alternate"><h3>coverflow!!!</h3></div>
-            </div>
-        )
-    }
-    else if(props.screen===2){
-        return(
-            <div className="screen">
-               <div className="alternate"><h3> Music!!!</h3></div>
-            </div>
-        )
-    }
-    else if(props.screen===3){
-        return(
-            <div className="screen">
-               <div className="alternate"><h3> Games!!!</h3></div>
-            </div>
-        )
-    }
-    else if(props.screen===4){
-        return(
-            <div className="screen">
-               <div className="alternate"><h3> Settings!!!</h3></div>
-            </div>
-        )
-    }
-  
 }
 export default Screen;
--- src/screeninside.js
+++ src/screeninside.js
@@ -3,11 +3,45 @@ import Left from'./left'
 import Right from './right'
 const Screeninside=(props)=>{
     //if-else
-    return(
-        <div className="screeninside">
-            <Left active={props.active} updateRender={props.updateRender}/>
-            <Right/>
-        </div>
-    )
+    if(props.screen===0){ 
+        return(
+            <div className="screeninside">
+                <Left active={props.active} updateRender={props.updateRender}/>
+                <Right/>
+            </div>
+        )
+    }
+    else if(props.screen===1){
+        return(
+            <div className="screeninside">
+               <div className="alternate"><h2>Coverflow</h2></div>
+               <div className="alternateimg one"></div>
+            </div>
+        )
+    }
+    else if(props.screen===2){
+        return(
+            <div className="screeninside">
+               <div className="alternate"><h2>Music</h2></div>
+               <div className="alternateimg two"></div>
+            </div>
+        )
+    }
+    else if(props.screen===3){
+        return(
+            <div className="screeninside">
+               <div className="alternate"><h2>Games</h2></div>
+               <div className="alternateimg three"></div>
+            </div>
+        )
+    }
+    else if(props.screen===4){
+        return(
+            <div className="screeninside">
+               <div className="alternate"><h2>Settings</h2></div>
+               <div className="alternateimg four"></div>
+            </div>
+        )
+    }
 }
 export default Screeninside;
